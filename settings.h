15fdf088981f485d95b2d7b5c6f3b60f0cc45c3b
Bugguim <34062609+Bugguim@users.noreply.github.com>
2018-10-03 14:08:00 +0200

    Verbosity, no multiple declarations

--- settings.h
+++ settings.h
@@ -1,3 +1,14 @@
+/*********************************************************/
+/* File that groups all settings specific for each setup */
+/*********************************************************/
+#ifndef __TRUCKONTROL_SETTINGS_H__
+#define __TRUCKONTROL_SETTINGS_H__
+
+
 // I2C
 #define i2cMasterAddress 1
 #define pin_reloadDevices 2 // Interruption on that pin
+
+
+
+#endif //__TRUCKONTROL_SETTINGS_H__
