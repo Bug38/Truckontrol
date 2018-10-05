8573d9625b523e913312e6e8815a10627c1ba2fa
Lenaic Guimard <lenaic.guimardxx@nemera.net>
2018-10-05 09:19:30 +0200

    implementation for roadTractor

--- RoadTractor.h
+++ RoadTractor.h
@@ -5,7 +5,27 @@
 
 class RoadTractor {
   public:
+    // Build class instance
+    RoadTractor();
+    // Setup pins
+    void initPins(uint8_t motor, uint8_t servo);
+    void initPins(uint8_t motor, uint8_t servo, uint8_t front_lights);
+    void initPins(uint8_t motor, uint8_t servo, uint8_t front_lights, uint8_t rear_lights);
+    // Movements
+    void move(uint8_t speed, uint8_t direction);
+    void setSpeed(uint8_t speed);
+    void turn(uint8_t direction);
+    // Misc
+    void setLight(uint8_t lights);
+    
   private:
+    uint8_t front_pin_lights;
+    uint8_t rear_pin_lights;
+    uint8_t pin_motor;
+    uint8_t pin_servo;
+
+    int8_t current_speed;
+    int8_t current_direction;
 };
 
 #endif
